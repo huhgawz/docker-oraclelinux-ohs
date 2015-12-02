6e3c07a079e6df0082f0d02b9111d97776c8026f
Antonio Aguilar <antonio.aguilar@oracle.com>
2015-12-02 14:59:17 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-su - /bin/bash oracle -c "./oracle/Middleware/Oracle_WT1/opmn/bin/opmnctl startall"
+su - /bin/bash oracle -c "cd /oracle/Middleware/Oracle_WT1/opmn/bin && ./opmnctl startall"
